d732015959652eb52089aeafa8e6c551dab648e9
YagoGefaellOficial <yaguetegpl@gmail.com>
2025-04-23 18:31:13 +0200

    chaged the page controller view.

--- src/main/java/com/mtg/magicapi/Card/CardController.java
+++ src/main/java/com/mtg/magicapi/Card/CardController.java
@@ -1,56 +1,43 @@
 package com.mtg.magicapi.Card;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-@RestController
-@RequestMapping(path = "api/cards")
-public class CardController {
-    private final CardService cardService;
+@Controller
+@RequestMapping("/home")
+public class CardViewController {
 
     @Autowired
-    public CardController(CardService cardService) {
-        this.cardService = cardService;
+    private CardService cardService;
+
+    @GetMapping("/")
+    public String home(Model model, @RequestParam(defaultValue = "0") int page) {
+        Pageable pageable = PageRequest.of(page, 30);
+        Page<Card> allCards = cardService.getCards(pageable);
+        model.addAttribute("cards", allCards.getContent());
+        model.addAttribute("totalPages", allCards.getTotalPages());
+        model.addAttribute("currentPage", page);
+        return "home";
     }
 
-    @GetMapping
-    public Page<Card> getCards(
-            @RequestParam(required = false) Integer id,
-            @RequestParam(required = false) String name,
-            @RequestParam(required = false) String typeLine,
-            @RequestParam(required = false) String rarity) {
-        Pageable pageable = PageRequest.of(0, 30);
-
-        return cardService.getCards(pageable);
-    }
-
-    @PostMapping
-    public ResponseEntity<Card> addCard(@RequestBody Card card){
-        Card createdCard = cardService.addCard(card);
-        return new ResponseEntity<>(createdCard, HttpStatus.CREATED);
-    }
-
-    @PutMapping
-    public ResponseEntity<Card> updateCard(@RequestBody Card card){
-        Card resultCard = cardService.updateCard(card);
-        if (resultCard != null){
-            return new ResponseEntity<>(resultCard, HttpStatus.OK);
-        }
-        else{
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
-
-    @DeleteMapping("/{cardName}")
-    public ResponseEntity<String> deleteCard(@PathVariable String cardName){
-        cardService.deleteCard(cardName);
-        return new ResponseEntity<>("Card deleted succesfully", HttpStatus.OK);
+    @GetMapping("/search/")
+    public String searchCards(@RequestParam("name") String name, Model model, @RequestParam(defaultValue = "0") int page) {
+        Pageable pageable = PageRequest.of(page, 30);
+        Page<Card> cards = cardService.getCardsFromName(name, pageable);
+        model.addAttribute("cards", cards.getContent());
+        model.addAttribute("totalPages", cards.getTotalPages());
+        model.addAttribute("currentPage", page);
+        model.addAttribute("searchText", name);
+        return "home";
     }
 }
